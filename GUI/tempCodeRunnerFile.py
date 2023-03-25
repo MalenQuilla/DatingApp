898fd9f697da1df9d607d87f0b3109830b4e5f90
MalenQuilla <tungdv.bi12-476@st.usth.edu.vn>
2023-03-26 05:10:10 +0700

    new

--- GUI/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    # feframe_gender = tk.Button(frame_gender, image=female_img, bg="#FFFFFF", borderwidth=0, highlightthickness=0)
-    # les_button = tk.Button(frame_gender, image=les_img, bg="#FFFFFF", borderwidth=0, highlightthickness=0)
-    # gay_button = tk.Button(frame_gender, image=gay_img, bg="#FFFFFF", borderwidth=0, highlightthickness=0)
-    # bi_button = tk.Button(frame_gender, image=bi_img, bg="#FFFFFF", borderwidth=0, highlightthickness=0)
