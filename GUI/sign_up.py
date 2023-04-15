e9b52c9965dd70ddf53679b78b59f90778b43ac7
MalenQuilla <tungdv.bi12-476@st.usth.edu.vn>
2023-04-15 21:52:00 +0700

    new

--- GUI/sign_up.py
+++ GUI/sign_up.py
@@ -3,7 +3,7 @@ from tkinter import *
 from functools import partial
 from database_controller import show_account, insert_account, insert_matching, insert_info
 from GUI import secure
-from GUI import basics_interests, Drag_window
+from GUI import basics_interests
 
 class Sign_up:
     def __init__(self):
